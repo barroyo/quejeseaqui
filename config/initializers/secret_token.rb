06cbf9a17f51b426fd1d850395edd903f036f3c1
Cristopher Araya <caraya@avantica.net>
2012-09-21 20:53:17 -0600

    adding from scratch project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quejeseaqui::Application.config.secret_token = '53e9779f43267e8a2d1d1577b3d09ebb4cc87f5a31e4b92c4702fce43bc745102c5bb21084661daae807ace3f897f799719f78095ac3d5e84e80593a28e4deff'
